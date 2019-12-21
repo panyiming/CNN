5fbdfe1bd8e2a0a09ed670ffae9cd746b7a31f69
panyiming <405632709@qq.com>
2019-12-21 17:12:00 +0800

    [update] run shell

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 python main.py \
-    --imgs-path '/data1/dataset/face_train/test_cnn/data/train.ls' \
+    --imgs-path './data/train.ls' \
     --net 'testmodel' \
+    --model-name 'cnn_misnist' \
     --inshape '3,28,28' \
-    --save-root '/data1/dataset/face_train/test_cnn/' \
+    --save-root './models/' \
     --batch-size 128 \
-    --epoch 30 \
+    --epoch 20 \
     --class-num 10 \
-    --lr 0.0001 \
-    --model-path '/data1/dataset/face_train/test_cnn/model_name-0001.json' \
-    --step-epoch '10,20,30' \
+    --lr 0.001 \
+    --step-epoch '5,10,15' \
--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,7 @@
 python main.py \
-    --imgs-path '/data1/dataset/face_train/test_cnn/data/test.ls' \
+    --imgs-path './data/test.ls' \
     --inshape '3,28,28' \
     --batch-size 64 \
-    --model-path '/data1/dataset/face_train/test_cnn/model_name-0003.json' \
+    --model-path './models/cnn_misnist-0000.json' \
     --train 0 \
     --class-num 10 \
